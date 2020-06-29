bdbe636620af10f66b5135ec2efade40de01df0a
Karolina <kj.wozniczka@gmail.com>
2020-06-29 11:43:26 +0200

    tworzenie harmonogramu

--- src/main/frontend/src/component/Schedules/PersonalSchedule.js
+++ src/main/frontend/src/component/Schedules/PersonalSchedule.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda} from '@syncfusion/ej2-react-schedule';
+import { Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda,
+EventSettingsModel, ViewsDirective, ViewDirective} from '@syncfusion/ej2-react-schedule';
+// in case to add apointments to empty scheduler we need to import EventSettingModel
 
-
+import {DataManager, WebApiAdaptor} from '@syncfusion/ej2-data'
 export default class PersonalSchedule extends Component{
     constructor(props){
         super(props)
@@ -10,7 +12,12 @@ export default class PersonalSchedule extends Component{
     render(){
 
         return(
-            <ScheduleComponent>
+            <ScheduleComponent currentView='Month'>
+                <ViewsDirective>
+                    
+                    <ViewDirective option="Month" isSelected={true} ></ViewDirective>
+                    <ViewDirective option="Week" firstDayOfWeek="1"></ViewDirective>
+                </ViewsDirective>
                 <Inject services={[Day, Week, WorkWeek, Month, Agenda]}/>
             </ScheduleComponent>
         )
